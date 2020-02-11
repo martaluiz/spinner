da59cb8a70d443df92ac495782630ecd824bd087
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-11 20:18:45 +0000

    Spinner I

---
+++ spinner1.js
@@ -0,0 +1,38 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1500);
+
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 2000);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2500);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 3000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 3500);
+
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 4000);
